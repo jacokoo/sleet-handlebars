87ef89a8a47a4d81999eeee04db5cdf8929fcbf3
Jaco Koo <jaco.koo@guyong.in>
2017-08-31 11:22:03 +0800

    what are these changes?

--- src/compiler/block-setting.js
+++ src/compiler/block-setting.js
@@ -2,19 +2,13 @@ import { SettingCompiler } from 'sleet/lib/compiler/setting';
 import { compileAttributes } from './util';
 
 export class BlockSettingCompiler extends SettingCompiler {
-    compile (context, group, tag, note) {
-        const snote = context.getNote('block-setting');
-        super.compile(context, group, tag, snote);
-
-        const result = [`{{#${group.setting.name}`];
+    compile (context, group, tag) {
+        context.push(` {{#${group.setting.name}`);
         if (group.setting.attributes) {
             const attr = compileAttributes(context, group.setting.attributes);
-            result.push(`${attr}}}`);
+            context.push(`${attr}}}`);
         }
-
-        snote.each((key, value) => result.push(value === null ? ` ${key}` : ` ${key}="${value}"`));
-        result.push(`{{/${group.setting.name}}}`);
-        note.set(result.join(''), null);
-        snote.clear();
+        super.compile(context, group, tag);
+        context.push(`{{/${group.setting.name}}}`);
     }
 }
